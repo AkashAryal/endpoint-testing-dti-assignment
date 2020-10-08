2c62e7cdd4c2626d714c80cfb92b71776d6dc18f
Akash Aryal <aryal.akash4414@gmail.com>
2020-10-08 10:35:15 -0500

    added jest

---
+++ src/base.test.ts
@@ -0,0 +1,9 @@
+
+it("2+2===4", () => {
+  expect(2 + 2).toBe(4);
+});
+
+
+it('Testing to see if Jest works', () => {
+  expect(1).toBe(2)
+})
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { PORT, app } from "./server";
 
 app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!`);
 });
